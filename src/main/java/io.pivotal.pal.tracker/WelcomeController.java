76fb25fb9377b7b155abc3765f894d59577f7669
HCLTECH\zahid.s <zahid.s@hcl.com>
2018-09-17 16:16:10 +0530

    Testing App

--- src/main/java/io.pivotal.pal.tracker/WelcomeController.java
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -1,15 +1,26 @@
 package io.pivotal.pal.tracker;
 
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String welcomeMessage;
+
+
+    public WelcomeController( @Value("${welcome_message}") String welcomeMessage ) {
+
+        this.welcomeMessage = welcomeMessage ;
+
+    }
+
+
+
     @GetMapping("/")
     public String sayHello(){
 
-        return "Hello World";
+        return welcomeMessage;
     }
 }
